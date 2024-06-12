69bab1da092c5758e72570f546df34376ed4533b
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2024-06-12 19:59:40 +0300

    #Delete all the shapes #Add addEmployee page #Add the Search page #Update the css for the navBar #Add home page

--- HR_Web/Controllers/HomeController.cs
+++ HR_Web/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 using HR_Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using HR_Web.Util;
+
+
+
 
 namespace HR_Web.Controllers
 {
@@ -28,5 +32,65 @@ namespace HR_Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            if (string.IsNullOrEmpty(employee.FirstName) || string.IsNullOrEmpty(employee.LastName) || string.IsNullOrEmpty(employee.Email) || string.IsNullOrEmpty(employee.Phone))
+            {
+                ViewBag.ErrorMessage = "Please fill all fields.";
+                return View();
+            }
+            if (!new EmailValidator().ValidateEmail(employee.Email) || !new PhoneValidator().ValidatePhoneNumber(employee.Phone))
+            {
+                ViewBag.ErrorMessage = "Invalid email or phone number.";
+                return View();
+            }
+
+            using (var db = new HRContext())
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+            }
+            return View();
+        }
+
+        
+        public IActionResult SearchEmployee(string searchQuery)
+        {
+            if (string.IsNullOrEmpty(searchQuery))
+            {
+                return View("Search", Enumerable.Empty<Employee>());
+            }
+
+            var queryParts = searchQuery.Trim().Split();
+            IQueryable<Employee> employeesQuery = new HRContext().Employees;
+
+            if (queryParts.Length == 2)
+            {
+                var firstName = queryParts[0];
+                var lastName = queryParts[1];
+                employeesQuery = employeesQuery.Where(e =>
+                    e.FirstName.Contains(firstName) &&
+                    e.LastName.Contains(lastName)
+                );
+            }
+            else
+            {
+                employeesQuery = employeesQuery.Where(e =>
+                    e.FirstName.Contains(searchQuery) ||
+                    e.LastName.Contains(searchQuery)
+                );
+            }
+
+            var employees = employeesQuery.ToList();
+            return View("Search", employees);
+        }
+
     }
 }
--- HR_Web/Models/Employee.cs
+++ HR_Web/Models/Employee.cs
@@ -8,36 +8,36 @@
         public string Email { get; set; }
         public string Phone { get; set; }
         
-        public double salary { get; set; }
-        public int vacationDays { get; set; }
-        public int aprrovedVacationDays { get; set; }
+        public double Salary { get; set; }
+        public int VacationDays { get; set; }
+        public int ApprovedVacation{ get; set; }
 
 
 
-        public string material { get; set; }
-        public string gender { get; set; }
-        public DateTime birthDate { get; set; }
-        public string address { get; set; }
+        public string Material { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
 
 
         public Employee()
         {
         }
 
-        public Employee(int id, string firstName, string lastName, string email, string phone, double salary, int vacationDays, int aprrovedVacationDays, string material, string gender, string dob, string address)
+        public Employee(int id, string firstName, string lastName, string email, string phone, double salary, int vacationDays, int approvedVacation, string material, string gender, string dob, string address)
         {
             this.Id = id;
             this.FirstName = firstName;
             this.LastName = lastName;
             this.Email = email;
             this.Phone = phone;
-            this.salary = salary;
-            this.vacationDays = vacationDays;
-            this.aprrovedVacationDays = aprrovedVacationDays;
-            this.material = material;
-            this.gender = gender;
-            this.birthDate = DateTime.Parse(dob);
-            this.address = address;
+            this.Salary = salary;
+            this.VacationDays = vacationDays;
+            this.ApprovedVacation = approvedVacation;
+            this.Material = material;
+            this.Gender = gender;
+            this.BirthDate = DateTime.Parse(dob);
+            this.Address = address;
         }
 
     }
